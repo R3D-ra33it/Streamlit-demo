a724a342866a21e2a696b7886807ebe367c4df34
Ra33it <70468545+R3D-ra33it@users.noreply.github.com>
2025-01-31 15:53:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,27 @@ import streamlit as st
 
 api_key = st.secrets["API_KEY"]
 st.write(api_key)
+
+
+st.set_page_config(
+    page_title="AIS AI Coding Assistant",
+    page_icon="🤖",
+    layout="centered"
+)
+
+st.title('AIS AI Coding Assistant')
+
+prompt = st.chat_input('Ask anything...')
+
+if "chats" not in st.session_state:
+    st.session_state.chats = []
+
+if prompt:
+    st.session_state.chats.append({
+        "role": "user",
+        "content": prompt
+    })
+
+
+for chat in st.session_state.chats:
+    st.chat_message(chat['role']).markdown(chat['content'])
